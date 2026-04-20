1bfc3f348862783706781a8624dc508292456781
Eric Cogen <ericcogen@gmail.com>
2026-04-20 19:21:39 -0400

    [DOCS] Fix unclosed div JSX parse errors in compare and rules pages

--- site/app/compare/gauntletci-vs-codeql/page.tsx
+++ site/app/compare/gauntletci-vs-codeql/page.tsx
@@ -295,6 +295,7 @@ export default function VsCodeQLPage() {
         </section>
 
       </div>
+    </div>
     </>
   );
 }
--- site/app/compare/gauntletci-vs-semgrep/page.tsx
+++ site/app/compare/gauntletci-vs-semgrep/page.tsx
@@ -213,6 +213,7 @@ export default function SemgrepComparePage() {
         </div>
 
       </div>
+    </div>
     </>
   );
 }
